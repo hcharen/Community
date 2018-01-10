c524250b41ec5ac3127b7dfde645779ce72f67ce
Hillary Charen <hillary.charen@gmail.com>
2018-01-10 18:02:19 -0500

    action cable is now working, still have several kinks to sort out. Currently, you can create messaeges, I had to replace the form in _conversation.html.erb with the original ruby tags form, I added a second hidden field for conversation.id. This was the only difference between the new form and the original. After this change, messages showed up again in the views and saved to the database

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,24 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    # This is called AFTER conection
+    # It assigns the 'current_user' variable an instance of a logged user
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    protected
+
+    # Devise uses Warden, which assigns env['warden'] ALL INFORMATION about the session and authentication
+    # about a specific user
+    # Connection with the server will not be authorized unless the user is LOGGED-IN!
+    def find_verified_user
+      if (current_user = env['warden'].user)
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
---
+++ app/channels/conversation_channel.rb
@@ -0,0 +1,28 @@
+class ConversationChannel < ApplicationCable::Channel
+
+  # adds current_user(us), or single user, whatever to a specific channel. Users within a specifc channel will # recieve code/notifications from this channel. Those users who are not apart of that(this?) specific channel # will NOT recieve it's code
+  # creates a UNIQUE CHANNEL for each user
+  # The sender and recipient will recieve different information, so you'll always have to notify at least two
+  # channels.
+  def subscribed
+    stream_from "conversations-#{current_user.id}"
+  end
+
+  # removes users from a specifc channel. Once removed, they will no longer recieve code from that specific
+  # channel
+  # removes all connections!
+  def unsubscribed
+    stop_all_streams
+  end
+  # this creates a record and later will render a partial of a view, send an email, or log some infomation
+  # this method can be named whatever you want!
+  # builds a hash that based on passed data and sends data to the front-end using the
+  # ActionCable.server.broadcast method to the SPECIFIC CHANNEL!
+  # data sent from this method is visible in the RECIEVED method on the front-end
+  def speak(data)
+    message_params = data['message'].each_with_object({}) do |el, hash|
+      hash[el.values.first] = el.values.last
+    end
+  end
+
+end
