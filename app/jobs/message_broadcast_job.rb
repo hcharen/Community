2c3e15240665726c3fb99cd09624964af0113f1c
Hillary Charen <hillary.charen@gmail.com>
2018-01-10 18:55:04 -0500

    chat now works with non ruby render tag form and uncommented out after_create method in message.rb. Now, when you create a new message and send it, you dont have to scroll down to see the latest message. How did I accomplish this? I changed the conversation.coffee file back into a conversation.js file. Why does it work? Who the fuck knows.

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,7 +1,41 @@
 class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  # main method of the class
+  # all code within this method will be executed when perform_later method is called
+  def perform(message)
+    sender = message.user
+    recipient = message.conversation.opposed_user(sender)
+
+    # these guys connect with a specific channel and send a conversation_id and HTML code using a partial!
+    broadcast_to_sender(sender, message)
+    broadcast_to_recipient(recipient, message)
+  end
+
+  private
+
+  # in rails 5, you can render HTML code from any partial or view by calling the ApplicationController.render
+  # method OUTSIDE of a controller! - HOW COOL AND EXCITING!!
+  def broadcast_to_sender(user, message)
+    ActionCable.server.broadcast(
+      "conversations-#{user.id}",
+      message: render_message(message, user),
+      conversation_id: message.conversation_id
+    )
+  end
+
+  def broadcast_to_recipient(user, message)
+    ActionCable.server.broadcast(
+      "conversations-#{user.id}",
+      message: render_message(message, user),
+      conversation_id: message.conversation_id
+    )
+  end
+
+  def render_message(message, user)
+    ApplicationController.render(
+      partial: 'messages/message',
+      locals: { message: message, user: user }
+    )
   end
 end
